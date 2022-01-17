24392f51b0f7e6aad8d85f057e106ffb97fea845
Andreia Rodrigues <andreiarodrigues@MacBook-Pro-de-Andreia.local>
2022-01-17 16:03:08 +0100

    Feat: adding the stylesheet

---
+++ public/gpt/gpt.js
@@ -0,0 +1,15 @@
+// this function talks to the local GPT robot server
+async function talkToRobot(prompt) {
+
+	// Se connecter au serveur GPT local
+	const request = await fetch("http://10.42.0.1:8000/ask;'" + prompt + "'", {
+		method: 'GET'
+	})
+
+	console.log("asking GPT for new text")
+	const response = await request.text()
+	
+	console.log("gpt result received:")
+	console.log(response)
+
+}
---
+++ public/sketch.js
@@ -0,0 +1,46 @@
+// la taille de notre objet
+var taille = 0
+var canvas;
+
+// créer un canvas plein écran du browser
+function setup() {
+	canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0,0, 'fixed');
+    canvas.style('z-index', '-1')
+}
+
+function windowResized(){
+
+    //if (windowWidth > 600){
+    resizeCanvas(windowWidth, windowHeight);
+       // }else{
+        //    resizeCanvas(windowWidth, windowHeight/2);
+      //  }
+}
+
+function draw() {
+	// effacer les pixels précédents
+	clear()
+	// couleur
+	fill(230,200,20)
+	noStroke()
+	// dessiner cercle au milieu avec cette variable "taille"
+	push();
+	circle(width*0.5, height*0.5, taille-1)
+	pop();
+
+}
+
+function keyPressed() {
+	if (key == 'r') {
+		talkToRobot("Hello Robot")
+	}
+}
+
+// lorsqu'il y a un changement de la roue de la souris
+function mouseWheel(event) {
+	// extraire uniquement le changement (delta == changement) sur l'axe Y
+	let deltaY = event.wheelDeltaY
+	// faire une transition de l'ancienne valeur de taille vers le nouveau deltaY
+	taille = lerp(taille,deltaY,0.1)
+}
