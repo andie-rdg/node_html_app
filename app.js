dbb7bd1cad329f893ad6d64a6c9b7122bbcf8114
Andreia Rodrigues <andreiarodrigues@MacBook-Pro-de-Andreia.local>
2022-01-17 17:40:16 +0100

    database connection

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const moment = require("moment");
+const {MongoClient} = require("mongodb");
+
 var path = require('path');
 const port = 3000;
 var bodyParser = require('body-parser')
 const app = express();
+
+const config = require('./config.js');
   
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
@@ -10,7 +15,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/', urlencodedParser, (req, res) => {
     app.use(express.static(__dirname + '/public'));
-    console.log("From the server:", req.body.data);
+
+    let toSend = {
+      message: req.body.answer,
+      date: moment().format()
+    }
+
+    console.log("Send from the server:", toSend);
+
+    //connect to my mongodb w/ connectionurl
+     MongoClient.connect(config.MONGO_DB_CONNECTION_URL,
+      {useUnifiedTopology: true},
+      (err, db) => {
+      if (err) throw err;
+      let databaseMongo = db.db(config.MONGO_DB_NAME);
+  
+      // Connect to the meme entry collection of the db
+      // Insert the data;
+      databaseMongo.collection(config.MONGO_DB_COLLECTION).
+      insertOne({
+        data: toSend,
+      }).then(() => {
+  
+        console.log("this data had been inserted", toSend);
+
+      })
+    }); 
+
 });
 
 app.listen(port);
